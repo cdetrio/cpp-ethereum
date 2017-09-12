9e0d08b4cf631a2bfbcff5451e4701199057120d
cdetrio <cdetrio@gmail.com>
2017-09-11 23:54:09 -0400

    fix long int

--- libethcore/SealEngine.h
+++ libethcore/SealEngine.h
@@ -85,7 +85,7 @@ public:
 		printf("SealEngine.h isPrecompiled address _a: %s\n", _a.hex().c_str());
 		printf("m_params.precompiled.at(_a).startingBlock(): %s\n", boost::lexical_cast<std::string>(m_params.precompiled.at(_a).startingBlock()).c_str());
 		printf("_blockNumber: %s\n", boost::lexical_cast<std::string>(_blockNumber).c_str());
-		printf("m_params.precompiled.count(_a): %d\n", m_params.precompiled.count(_a));
+		printf("m_params.precompiled.count(_a): %lu\n", m_params.precompiled.count(_a));
 		return m_params.precompiled.count(_a) != 0 && _blockNumber >= m_params.precompiled.at(_a).startingBlock();
 	}
 	virtual bigint costOfPrecompiled(Address const& _a, bytesConstRef _in, u256 const&) const { return m_params.precompiled.at(_a).cost(_in); }
